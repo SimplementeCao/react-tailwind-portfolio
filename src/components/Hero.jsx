421c33c99b3d5aad5e967b6d6459b9b82c385604
LeandroCarvajal <carvajal.leandro.94@gmail.com>
2024-12-10 01:08:47 -0300

    Fix + Add motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,50 @@
 import { HERO_CONTENT } from "../ constants";
-import perfil from "../assets/sousou.jpg"
+import perfil from "../assets/sousou.jpg";
+import {motion} from "framer-motion";
+
+const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.5, delay: delay} 
+    }
+})
 
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 flex items-center pb-20 lg:mb-35">
         <div className="flex flex-wrap items-center w-full">
             <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start lg:gap-5">
-                    <h1 className="pb-20 text-5xl font-thin tracking-tight 
+                <div className="flex flex-col items-center lg:items-start lg:gap-3">
+                    <motion.h1 
+                    variants={container(0)}
+                    initial="hidden"
+                    animate="visible"
+                    className="pb-20 text-5xl font-thin tracking-tight 
                     lg:mt-16 lg:text-8xl">
                         Leandro Cesar Carvajal
-                    </h1>
-                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text 
-                    text-4xl tracking-tight text-transparent lg:mt-20 lg:text-4xl">
+                    </motion.h1>
+                    <motion.span 
+                    variants={container(0.5)}
+                    initial="hidden"
+                    animate="visible"
+                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text 
+                    text-3xl tracking-tight text-transparent lg:mt-20 lg:text-4xl">
                         Full Stack Developer
-                    </span>
-                    <p className="my-2 max-w-xl font-light tracking-tight">
+                    </motion.span>
+                    <motion.p
+                    variants={container(0.75)}
+                    initial="hidden"
+                    animate="visible" 
+                    className="my-2 max-w-xl font-light tracking-tight">
                         {HERO_CONTENT}
-                    </p>
+                    </motion.p>
                 </div>
             </div>
             <div className="w-full lg:w-1/2 lg:p-2">
                 <div className="flex justify-center">
-                    <img src={perfil} alt="Leandro Cesar CarvajalclassName=" h-64 w-64 rounded-full object-cover shadow-lg lg:h-80 />
+                    <img src={perfil} alt="Leandro Cesar Carvajal className=" h-64 w-64 rounded-full object-cover shadow-lg lg:h-80 />
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
       <div></div>
       <div className="flex items-center gap-6">
         <a
-          href="CV ES - Leandro Carvajal.pdf"
-          download="CV ES Fullstack Developer - Carvajal Leandro.pdf"
+          href="CV ES Leandro Carvajal.pdf"
+          download="CV ES Carvajal Leandro.pdf"
           className="rounded bg-blue-500 px-4 py-2 text-white shadow hover:bg-blue-700"
         >
-          Descargar CV
+          Download CV
         </a>
         <div className="flex items-center justify-end gap-4 text-2xl">
         <a
